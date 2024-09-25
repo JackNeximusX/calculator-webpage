f0a488a6aea0fcc3cd23d94987599f246b1c7096
neximus <liamwattley15@gmail.com>
2024-09-25 08:22:30 -0500

    deleted unnecesary buttons

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+
+// console.dir(button);
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+
+function clear() {
+    display.value = "";
+}
+
+
+function calculate() {
+
+}
+
+display.log( clear())
