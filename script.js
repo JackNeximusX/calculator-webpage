9f6b9809e7d8410ddf0415a336a8de216f171ae5
neximus <liamwattley15@gmail.com>
2024-11-15 08:28:57 -0600

    wip

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
+
+
 const display = document.getElementById("display");
-let x = "0";
 display.value = "0";
+
+let firstValue = null;
+let secondValue = null;
 let y = null;
 let operator = null;
 
+
+
+
+// when button with this function is clicked
 function addDigit(input) {
-    if (x === "0") {
-        x = input;
+    // if the current value is null or equal to 0
+    if (firstValue === null && firstValue === 0) {
+        //give the string value of that input
+       firstValue = input;
     } else {
-        x += input;
+        //add the string value of that input
+       firstValue += input;
     }
-    display.value = x;
+    // display the string value of the current value
+    display.value = firstValue;
+
+    // set y as the string value of the current value
+    y = firstValue;
+
+    // set the current value of first value as a number
+    firstValue = parseFloat(firstValue);//contenue here
 }
 
 function decimalCheck(input) {
-    if (!x.includes(".")) {
-        if (x === "0" && input !== ".") {
-            x = input;
+    if (!currentValue.includes(".")) {
+        if (currentValue === "0" && input !== ".") {
+           currentValue = input;
         } else {
-            x += input;
+           currentValue += input;
         }
-        display.value = x;
+        display.value = currentValue;
     }
 }
 
@@ -29,10 +47,17 @@ function operationCheck(input) {
         calculation();
     }
 
-    operator = input;
-    y = parseFloat(x); 
-    x = "0"; 
-    display.value = x;
+    if (currentValue !== "0"){
+        operator = input;
+        y = parseFloat(x); 
+       currentValue = "0"; 
+        display.value = y;
+        console.log("currentValue not 0");
+    }
+    else{
+        display.value = y;
+    }
+    
 }
 
 function calculation() {
@@ -58,24 +83,25 @@ function calculation() {
         }
 
         display.value = result;
-        x = result.toString();
+       currentValue = result.toString();
         y = result;
         operator = null;
     }
 }
 
 function clearDisplay() {
-    x = "0";
+   currentValue = "0";
     display.value = "0";
     y = null;
     operator = null;
 }
 
 function deleteDigit() {
-    if (x.length > 1) {
-        x = x.slice(0, -1);
+    if (currentValue.length > 1) {
+       currentValue =currentValue.slice(0, -1);
     } else {
-        x = "0";
+       currentValue = "0";
     }
-    display.value = x;
+    display.value =currentValue;
 }
+// TODO: fix multi opporation bug, and make currentValue number
