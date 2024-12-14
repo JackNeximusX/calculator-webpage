5ecac51d0c0f63a0648c90926af61b60ff1deaa7
neximus <liamwattley15@gmail.com>
2024-12-13 19:14:58 -0600

    simplified addOperator and addDigit, moved deletDigit function back to bottom

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 const display = document.getElementById("display");
 
-let currentOpr = null;//string
-let currentValue = 0;//number
+let currentOpr = null;
+let currentValue = 0;
 display.value = "0";
 let hasResult = false;
 
-//check what side of the operation we currently on 
+
 
 
 function addDigit(input){
     
-
-
-
-    if(!currentOpr && hasResult === true){
+    if( hasResult || display.value === "0"){
         display.value = input;
-    } else if(currentOpr && currentValue == display.value){
-        display.value = input;
-    } else if(display.value === "0"){
-        display.value = input;
+        hasResult = false;
     } else {
         display.value += input;
     }
 
-
-
-    // == verses two difreent types
     if (!currentOpr && currentValue != display.value) {
         currentValue = parseFloat(display.value);
     }
-    //
-    // if((!currentOpr && currentValue) || (currentValue === 0)){
-    //     currentValue = parseFloat(display.value);
-    // }
 
-function deleteDigit(){
-    display.value = display.value.slice(0,-1);
-}
-
-    console.log(currentValue, currentOpr, display.value);
+    console.log(currentValue, currentOpr, display.value, hasResult);
 
 }
 
@@ -51,12 +34,12 @@ function addDecimal(input){
 }
 
 function addOperator(input){
-    if(currentOpr === null){
+    if(!currentOpr){
         currentOpr = input;
-        hasResult = false;
+        hasResult = true;
     }
 
-    console.log(currentValue, currentOpr, display.value);
+    console.log(currentValue, currentOpr, display.value, hasResult);
 }
 
 
@@ -86,21 +69,23 @@ function calculate(){
         currentValue = result;
         currentOpr = null;
         hasResult = true;
-        console.log(currentValue, currentOpr, display.value);
+
+        console.log(currentValue, currentOpr, display.value, hasResult);
 
     }
 }
 
-
-//set a variable as a operator and get the ready for second half of equaion
-//if both all parts of the equasion is ready run the equasion
-
-//is clear is pressed clear everything
 function clearDisplay(){
     display.value = "0";
     currentValue = 0;
     currentOpr = null;
     hasResult = false;
+
+    console.log(currentValue, currentOpr, display.value, hasResult);
 }
 
-//if d is pressed delete on character
+function deleteDigit(){
+    display.value = display.value.slice(0,-1);
+
+    console.log(currentValue, currentOpr, display.value, hasResult);
+}
